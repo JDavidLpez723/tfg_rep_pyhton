d88e28e0f6b2efe03fcbec175d3219552ac253d7
JDavidLpez723 <100363865@alumnos.uc3m.es>
2022-04-22 19:47:34 +0200

    download, and extract files in a loop, the store data in a list

--- new.py
+++ new.py
@@ -1,12 +1,87 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+#----------------------------------------------------------------IMPORTS
+
 import os
 import pandas as pd
 import requests as rq
+import gzip as gz
+import shutil as sh
+import csv
 
 
-df = pd.read_csv("input_data/myfile.tsv", sep='\t') #open the input file as a dataframe
-df = df.drop(df.columns[[1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33]],axis = 1) #drops all columns except 'id' and 'gas_used'
+#----------------------------------------------------------------VARIABLE DECLARATION
+
+year = 2015     #year for the url of the input file
+month = 7
+day = 30
+url1 = 'https://gz.blockchair.com/ethereum/blocks/blockchair_ethereum_blocks_'   #the first part of the url of the input file
+ext = '.tsv.gz' #extension of the file
 
 
-print(df)
+
+global_block_list = []  #the list where all the block data from all the input files is going to be stored
+
+
+
+aa = 0
+while aa < 2:
+    #---------------------------------------------------------------DOWNLOAD INPUT FILE
+    
+    #we adapt the file id
+    if month < 10:
+        if day < 10:
+            file_id = str(year)+"0"+str(month)+"0"+str(day)   
+        else:
+            file_id = str(year)+"0"+str(month)+str(day)   
+        if day < 10:
+            file_id = str(year)+str(month)+"0"+str(day)  
+        else:
+            file_id = str(year)+str(month)+str(day)   
+
+
+    url = url1 + file_id + ext #the url of the input file
+
+    r = rq.get(url, allow_redirects=True)
+    open('input_data/input_file.tsv.gz', 'wb').write(r.content)    #the .gz file is downloaded
+
+
+    #--------------------------------------------------------------EXTRACT INPUT FILE
+
+    with gz.open('input_data/input_file.tsv.gz', 'rb') as f_in:    #the .gz file is extracted and a .tsv file is obtained
+        with open('input_data/input_file.tsv', 'wb') as f_out:
+            sh.copyfileobj(f_in, f_out)
+
+    os.remove("input_data/input_file.tsv.gz")   #the .gz file is deleted after extraction
+
+
+    #---------------------------------------------------------------OPEN INPUT FILE
+
+    input_file = open("input_data/input_file.tsv")
+    input_file_list = csv.reader(input_file, delimiter="\t")
+
+    for row in input_file_list:
+        del row
+        break
+
+    for row in input_file_list:
+        del row[1:7]
+        del row[2:28]
+        global_block_list.append(row)
+        print(row)
+
+
+    aa+=1
+    day+=1
+    if day > 31:
+        day = 1
+
+
+    #df = pd.read_csv("input_data/input_file.tsv", sep='\t') #the .tsv input file is opened as a dataframe
+    #df = df.drop(df.columns[[1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33]],axis = 1) #all columns are dropped except 'id' and 'gas_used'
+
+
+    #print(df)
+
+
